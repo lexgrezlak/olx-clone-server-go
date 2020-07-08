899c0f47a11cf07f37e0691f10269a138845f908
John <accs.x@pm.me>
2020-07-08 22:46:42 +0200

    Connect to the database in the main

--- main/main.go
+++ main/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"olx-clone-server/database"
+	"os"
 )
 
 
@@ -10,11 +12,25 @@ func main() {
 	var err error
 
 	c := database.Config{}
+	ctx := context.Background()
 	if err = database.LoadConfig("env.json", "database", &c); err != nil {
 		panic(err)
 	}
 
-	fmt.Println(c)
+	db, err := database.NewFromEnv(ctx, &c)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close(ctx)
+
+	var email string
+	err = db.Pool.QueryRow(ctx, "SELECT email FROM user").Scan(&email);
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	}
+
+	fmt.Println(email)
+
 
 	//dbpool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	//if err != nil {
