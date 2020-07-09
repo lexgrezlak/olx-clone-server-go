5de7af314359e61817a46a432f8b36695ae4c0e5
John <accs.x@pm.me>
2020-07-09 04:27:54 +0200

    Add coverage()

---
+++ internal/utils/coverage.go
@@ -0,0 +1,10 @@
+package utils
+
+import "errors"
+
+func Coverage(condition bool) error {
+	if condition {
+		return errors.New("condition was set")
+	}
+	return nil
+}
---
+++ internal/utils/coverage_test.go
@@ -0,0 +1,25 @@
+package utils
+
+import "testing"
+
+func TestCoverage(t *testing.T) {
+	type args struct {
+		condition bool
+	}
+	testCases := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"no condition",
+			args{true},
+			true,
+		},
+	}
+	for _, tc := range testCases {
+		if err := Coverage(tc.args.condition); (err != nil) != tc.wantErr {
+			t.Errorf("Coverage() error = %v, wantErr %v", err, tc.wantErr)
+		}
+	}
+}
